0e9e1c777a6310a053af4a7fa50681d5c1bdb37b
Simen Bekkhus <sbekkhus91@gmail.com>
2016-10-05 15:23:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# prometheus-hystrix
+> Expose Hystrix stream as Prometheus data
+
+[![NPM Version][npm-image]][npm-url]
+[![Build Status][travis-image]][travis-url]
+
+[![Dependency Status][david-image]][david-url]
+[![Optional Dependency Status][david-optional-image]][david-optional-url]
+[![Peer Dependency Status][david-peer-image]][david-peer-url]
+[![Dev Dependency Status][david-dev-image]][david-dev-url]
+
+## Usage
+
+This module has a peer dependency on [`prom-client`]([prom-client-url]). Currently, version 3-5 are supported.
+
+This module exports a single function, taking a [`Stream`]([node-stream-api-url]) of [Hystrix data]([hystrix-data-stream-url]), and
+returning a [`Subscription`]([rxjs-url]) of the same data, which can be unsubscribed if you so which.
+
+## Metrics exposed
+
+This module exposes 17 metrics, all using the name of the `HystrixCommand` as the label:
+
+1. `hystrix_errors_total`: Total number of times the breaker has errored
+2. `hystrix_requests_total`: Total number of requests made to the breaker
+3. `hystrix_collapsed_requests_total`: Total number of times a requests has been collapsed.
+4. `hystrix_exceptions_thrown_total`: Total number of times an exception has been thrown.
+5. `hystrix_failures_total`: Total number of times a call has failed.
+6. `hystrix_fallback_failures_total`: Total number of times a fallback has failed.
+7. `hystrix_fallback_rejections_total`: Total number of times a fallback has rejected.
+8. `hystrix_fallback_successes_total`: Total number of times a fallback has been successful.
+9. `hystrix_responses_from_cache_total`: Total number of times a response has been returned from the cache.
+10. `hystrix_semaphore_rejections_total`: Total number of times a request failed because of empty semaphore.
+11. `hystrix_short_circuits_total`: Total number of times a request short circuited.
+12. `hystrix_successes_total`: Total number of times a request succeeded.
+13. `hystrix_thread_pool_rejections_total`: Total number of times a the thread pool rejected.
+14. `hystrix_timeouts_total`: Total number of times a request timed out.
+15. `hystrix_current_concurrent_executions_total`: Total number of requests going concurrently.
+16. `hystrix_latency_execute_mean`: Mean latency of all executions.
+17. `hystrix_latency_total_mean`: Mean latency of all requests.
+
+
+## TODO
+
+There are currently no metrics for the calculated percentiles `latencyExecute` and `latencyTotal`.
+
+
+[travis-url]: https://travis-ci.org/SimenB/node-prometheus-hystrix
+[travis-image]: https://img.shields.io/travis/SimenB/node-prometheus-hystrix.svg
+[npm-url]: https://npmjs.org/package/prometheus-hystrix
+[npm-image]: https://img.shields.io/npm/v/prometheus-hystrix.svg
+[david-url]: https://david-dm.org/SimenB/node-prometheus-hystrix
+[david-image]: https://img.shields.io/david/SimenB/node-prometheus-hystrix.svg
+[david-dev-url]: https://david-dm.org/SimenB/node-prometheus-hystrix#info=devDependencies
+[david-dev-image]: https://img.shields.io/david/dev/SimenB/node-prometheus-hystrix.svg
+[david-peer-url]: https://david-dm.org/SimenB/node-prometheus-hystrix#info=peerDependencies
+[david-peer-image]: https://img.shields.io/david/peer/SimenB/node-prometheus-hystrix.svg
+[david-optional-url]: https://david-dm.org/SimenB/node-prometheus-hystrix#info=optionalDependencies
+[david-optional-image]: https://img.shields.io/david/optional/SimenB/node-prometheus-hystrix.svg
+[prom-client-url]: https://github.com/siimon/prom-client
+[node-stream-api-url]: https://nodejs.org/api/stream.html
+[hystrix-data-stream-url]: https://github.com/Netflix/Hystrix/tree/master/hystrix-contrib/hystrix-metrics-event-stream
+[rxjs-url]: https://github.com/ReactiveX/rxjs
---
+++ hystrix-stream.js
@@ -0,0 +1,81 @@
+import { fromStream } from 'rx-node';
+import Gauge from 'prom-client/lib/gauge';
+
+const errorCount = new Gauge('hystrix_errors_total', 'Total number of times the breaker has errored.', ['breakerName']);
+const requestCount = new Gauge('hystrix_requests_total', 'Total number of requests made to the breaker', ['breakerName']);
+const rollingCountCollapsedRequests = new Gauge('hystrix_collapsed_requests_total', 'Total number of times a requests has been collapsed.',
+  ['breakerName']);
+const rollingCountExceptionsThrown = new Gauge('hystrix_exceptions_thrown_total', 'Total number of times an exception has been thrown.',
+  ['breakerName']);
+const rollingCountFailure = new Gauge('hystrix_failures_total', 'Total number of times a call has failed.', ['breakerName']);
+const rollingCountFallbackFailure = new Gauge('hystrix_fallback_failures_total', 'Total number of times a fallback has failed.',
+  ['breakerName']);
+const rollingCountFallbackRejection = new Gauge('hystrix_fallback_rejections_total', 'Total number of times a fallback has rejected.',
+  ['breakerName']);
+const rollingCountFallbackSuccess = new Gauge('hystrix_fallback_successes_total', 'Total number of times a fallback has been successful.',
+  ['breakerName']);
+const rollingCountResponsesFromCache = new Gauge('hystrix_responses_from_cache_total',
+  'Total number of times a response has been returned from the cache.', ['breakerName']);
+const rollingCountSemaphoreRejected = new Gauge('hystrix_semaphore_rejections_total',
+  'Total number of times a request failed because of empty semaphore.', ['breakerName']);
+const rollingCountShortCircuited = new Gauge('hystrix_short_circuits_total', 'Total number of times a request short circuited.',
+  ['breakerName']);
+const rollingCountSuccess = new Gauge('hystrix_successes_total', 'Total number of times a request succeeded.', ['breakerName']);
+const rollingCountThreadPoolRejected = new Gauge('hystrix_thread_pool_rejections_total',
+  'Total number of times a the thread pool rejected.', ['breakerName']);
+const rollingCountTimeout = new Gauge('hystrix_timeouts_total', 'Total number of times a request timed out.', ['breakerName']);
+const currentConcurrentExecutionCount = new Gauge('hystrix_current_concurrent_executions_total',
+  'Total number of requests going concurrently.', ['breakerName']);
+const latencyExecuteMean = new Gauge('hystrix_latency_execute_mean', 'Mean latency of all executions.', ['breakerName']);
+const latencyTotalMean = new Gauge('hystrix_latency_total_mean', 'Mean latency of all requests.', ['breakerName']);
+
+export default stream => fromStream(stream)
+  .map(data => data.replace(/^data: /, ''))
+  .map(JSON.parse)
+  .subscribe(({ name, ...data }) => {
+    errorCount.labels(name).set(data.errorCount);
+    requestCount.labels(name).set(data.requestCount);
+
+    rollingCountCollapsedRequests.labels(name).set(data.rollingCountCollapsedRequests);
+    rollingCountExceptionsThrown.labels(name).set(data.rollingCountExceptionsThrown);
+    rollingCountFailure.labels(name).set(data.rollingCountFailure);
+    rollingCountFallbackFailure.labels(name).set(data.rollingCountFallbackFailure);
+    rollingCountFallbackRejection.labels(name).set(data.rollingCountFallbackRejection);
+    rollingCountFallbackSuccess.labels(name).set(data.rollingCountFallbackSuccess);
+    rollingCountResponsesFromCache.labels(name).set(data.rollingCountResponsesFromCache);
+    rollingCountSemaphoreRejected.labels(name).set(data.rollingCountSemaphoreRejected);
+    rollingCountShortCircuited.labels(name).set(data.rollingCountShortCircuited);
+    rollingCountSuccess.labels(name).set(data.rollingCountSuccess);
+    rollingCountThreadPoolRejected.labels(name).set(data.rollingCountThreadPoolRejected);
+    rollingCountTimeout.labels(name).set(data.rollingCountTimeout);
+
+    currentConcurrentExecutionCount.labels(name).set(data.currentConcurrentExecutionCount);
+
+    latencyExecuteMean.labels(name).set(data.latencyExecute_mean);
+
+    latencyTotalMean.labels(name).set(data.latencyTotal_mean);
+  });
+
+// Missing data:
+/*
+ latencyExecute:
+ { '0': 100,
+ '25': 103,
+ '50': 104,
+ '75': 105,
+ '90': 105,
+ '95': 106,
+ '99': 106,
+ '100': 106,
+ '99.5': 106 },
+ latencyTotal:
+ { '0': 100,
+ '25': 103,
+ '50': 104,
+ '75': 105,
+ '90': 105,
+ '95': 106,
+ '99': 106,
+ '100': 106,
+ '99.5': 106 }}
+ */
